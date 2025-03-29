0cdb9eea68707879731a66bd649edd337a1eff6f
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-29 23:48:10 +0500

    rag model advance query using llm model for advancing queries

---
+++ RAG/RAG-Model_Using_LLM.py
@@ -0,0 +1,44 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
+from langchain.vectorstores import Chroma
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+import streamlit as st
+
+# Load documents
+loader = TextLoader("speech.txt")
+docs = loader.load()
+print(docs)  # Optional: just to see the loaded docs
+
+# Split into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+document = text_splitter.split_documents(docs)
+
+# Vectorstore with Ollama Embeddings
+db = Chroma.from_documents(document[:20], OllamaEmbeddings(model="llama2"))
+
+# Setup LLM
+llm = Ollama(model="llama2")
+
+# Setup Prompt Template
+prompt = ChatPromptTemplate.from_template("""
+answer the following question based on the provided context. Think carefully before answering, it's very high-quality data.
+<context>{context}</context>
+Question: {input}
+""")
+
+# Create chains
+document_chain = create_stuff_documents_chain(llm, prompt)
+retriever = db.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+# Query the chain
+def data_question(question):
+    return retrieval_chain.invoke({"input":{question}})
+st.title("RAG Question Answering base on Text file")
+question = st.text_input("Please enter your question:")
+if question:
+    st.write(data_question(question))
