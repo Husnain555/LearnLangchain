c568c48db4c89cbae6e6bc6ddccbde004606ca87
Husnain555 <husnain.babar.dev@gmail.com>
2025-04-09 23:59:42 +0500

    GRoq chat modal

--- groq/main.py
+++ groq/main.py
@@ -17,7 +17,7 @@ groq_api_key = os.environ["LANGCHAIN_GROQ_API_KEY"]
 if 'vector' not in st.session_state:
     st.session_state.embeddings = OllamaEmbeddings()
     st.session_state.loader = WebBaseLoader(
-        "https://coredirection.com/guides/home?_gl=1*1litgrk*_ga*MTQ4NDgzNDc2OC4xNzQ0MjIyOTE4*_ga_LVSSFZ40HP*MTc0NDIyMjk0Ni4xLjEuMTc0NDIyMjk1NC4wLjAuMA..")
+        "https://coredirection.com/guides/user-guide#challenges-leaderboards")
     st.session_state.docs = st.session_state.loader.load()
     st.session_state.chunks_documents = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     st.session_state.final_document = st.session_state.chunks_documents.split_documents(st.session_state.docs)
@@ -25,7 +25,7 @@ if 'vector' not in st.session_state:
 
 st.title("Chat Groq Demo")
 
-llm = ChatGroq(groq_api_key=groq_api_key, model="Gemma-7b-It")
+llm = ChatGroq(groq_api_key=groq_api_key, model="gemma2-9b-it")
 
 prompt = ChatPromptTemplate.from_template('''
 Use the provided context to answer the question as accurately as possible.
